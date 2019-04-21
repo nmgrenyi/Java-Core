ccae0827e8501ad3b40a048fd3ceeb39f5041e9e
nmgrenyi <Yi_Ren1993@163.com>
2019-04-21 16:26:20 -0700

    Enumeration Class initial

---
+++ src/enumberationDemo/EnumberaionTester.java
@@ -0,0 +1,28 @@
+package enumberationDemo;
+
+import java.util.Vector;
+import java.util.Enumeration;
+
+/**
+ * @author renyi
+ *
+ */
+public class EnumberaionTester {
+
+	public static void main(String[] args) {
+		Enumeration<String> days;
+		Vector<String> dayNames = new Vector<String>();
+		dayNames.add("Monday");
+		dayNames.add("Tuesday");
+		dayNames.add("Wednesday");
+		dayNames.add("Thursday");
+		dayNames.add("Friday");
+		dayNames.add("Saturday");
+		dayNames.add("Sunday");
+		days = dayNames.elements();
+		while (days.hasMoreElements()) {
+			System.out.println(days.nextElement());
+		}
+	}
+
+}
