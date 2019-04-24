b9703560d0e7915128fbfee73cd2ced505d3a837
nmgrenyi <Yi_Ren1993@163.com>
2019-04-23 23:40:50 -0700

    generic

---
+++ src/generics/GenericMethodTest.java
@@ -0,0 +1,28 @@
+package generics;
+
+public class GenericMethodTest {
+
+	public static <E> void printArray(E[] inputArray) {
+		for (E element : inputArray) {
+			System.out.printf("%s", element);
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		Integer[] intArray = {1, 2, 3, 4, 5};
+		Double[] doubleArray = {1.1, 2.2, 3.3, 4.4, 5.5};
+		Character[] charArray = {'H', 'E', 'L', 'L', 'O'};
+		
+		System.out.println("intArray: ");
+		printArray(intArray);
+
+		System.out.println("doubleArray: ");
+		printArray(doubleArray);
+
+		System.out.println("charArray: ");
+		printArray(charArray);
+
+	}
+
+}
